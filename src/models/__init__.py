d6a9f8f0518ca38bd69a160dfbbe4e4fb19354f3
shaanpatel-data <shaanpatel@gmail.com>
2026-03-11 11:19:42 -0700

    Add src/models/__init__.py with package docstring

---
+++ src/models/__init__.py
@@ -0,0 +1,6 @@
+"""
+Models package for estimating leg probabilities.
+
+This subpackage will contain modules implementing probability models used
+to estimate the likelihood of individual game outcomes.
+"""
